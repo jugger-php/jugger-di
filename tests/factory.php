863348a17a405d6fbbac3bee496123a3acb7bbf5
irpsv <i.rpsv@live.com>
2016-12-31 13:43:32 +0500

    create factory test

---
+++ tests/factory.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use jugger\di\Di;
+use jugger\di\Factory;
+
+class FactoryTest extends TestCase
+{
+    public function testBase()
+    {
+        Di::$f = new Factory();
+        Di::$f['a'] = "a";
+        Di::$f['b'] = function() {
+            return "b";
+        };
+
+        /*
+         * get test
+         */
+
+        $this->assertEquals(Di::$f['a'], "a");
+        $this->assertEquals(Di::$f['b'], "b");
+        $this->assertNull(Di::$f['d'], null);
+
+        /*
+         * set test
+         */
+
+        Di::$f['c'] = 123;
+        Di::$f['c'] = 456;
+
+        $this->assertEquals(Di::$f['c'], 123);
+    }
+}
