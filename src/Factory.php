a24b66322f2f211a7979d754cc91defd225605a9
irpsv <i.rpsv@live.com>
2016-12-31 12:53:00 +0500

    create Factory

---
+++ src/Factory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace jugger\di;
+
+use jugger\base\ArrayAccessTrait;
+
+/**
+ * Фабрика объектов
+ */
+class Factory implements \ArrayAccess
+{
+    use ArrayAccessTrait;
+
+    protected $vars = [];
+
+    public function __set(string $name, $value)
+    {
+        if (isset($this->$name)) {
+            return;
+        }
+        $this->vars[$name] = $value;
+    }
+
+    public function __get(string $name)
+    {
+        if (isset($this->$name)) {
+            $value = $this->vars[$name];
+            if ($value instanceof \Closure) {
+                return $value();
+            }
+            else {
+                return $value;
+            }
+        }
+        return null;
+    }
+
+    public function __isset(string $name)
+    {
+        return isset($this->vars[$name]);
+    }
+
+    public function __unset(string $name)
+    {
+        unset($this->vars[$name]);
+    }
+}
